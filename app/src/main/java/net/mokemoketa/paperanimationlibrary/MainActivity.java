920899b00c607376d649941463cceb843b76efae
mokemoketa <mokemoketa@gmail.com>
2016-10-25 15:17:48 +0900

    Changed version of AndroidStudio

--- app/src/main/java/net/mokemoketa/paperanimationlibrary/MainActivity.java
+++ app/src/main/java/net/mokemoketa/paperanimationlibrary/MainActivity.java
@@ -22,49 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         linearLayout = (LinearLayout) findViewById(R.id.mainframe);
-        //frameLayout = new FrameLayout(this);
-        //setContentView(frameLayout);
-        /*
-        basicCanvasView = new BasicCanvasView(this);
-        basicCanvasView.showCanvas(true);
-        linearLayout.addView(basicCanvasView);
-        PaperView test1 = new PaperView(this);
-        test1.setOrientation(LinearLayout.HORIZONTAL);
-        Button button1 = new Button(this);
-        button1.setText("1");
-        PaperView.LayoutParams test1params = new PaperView.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-        button1.setLayoutParams(test1params);
-        test1.addView(button1);
 
-        Button button2 = new Button(this);
-        button2.setText("2");
-        PaperView.LayoutParams test2params = new PaperView.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-        button2.setLayoutParams(test2params);
-        test1.addView(button2);
-        setContentView(test1);
-
-        LinearLayout test1 = new LinearLayout(this);
-        test1.setOrientation(LinearLayout.HORIZONTAL);
-        Button button1 = new Button(this);
-        button1.setText("1");
-        LinearLayout.LayoutParams test1params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-        button1.setLayoutParams(test1params);
-        linearLayout.addView(test1);
-        test1.addView(button1);
-*/
         final ImageView imageview1 = (ImageView)findViewById(R.id.imageView);
         imageview1.setImageResource(R.drawable.hedgehog);
 
         final PaperView paperview1 = new PaperView(this);//
         linearLayout.addView(paperview1);
 
-        //setContentView(imageview1);
         imageview1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
